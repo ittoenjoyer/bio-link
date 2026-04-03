c7500d6401be5a103d879bcee56ee03eb6ae463f
ittoenjoyer <minanokathia@gmail.com>
2026-04-03 16:20:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const toggle = document.getElementById('theme-toggle');
 toggle.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
   if (document.body.classList.contains('dark-mode')) {
-    toggle.textContent = '☀️ Modalità Giorno';
+    toggle.textContent = '☀️';
   } else {
-    toggle.textContent = '🌙 Modalità Notte';
+    toggle.textContent = '🌙 ';
   }
 });
